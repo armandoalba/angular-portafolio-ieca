7800b8bd8465f2e55ec942109afba498bb61f672
armandoalba <147946142+armandoalba@users.noreply.github.com>
2023-12-08 12:03:28 -0600

    Update ejercicio.js

--- javascript/2-objetos/ejercicio.js
+++ javascript/2-objetos/ejercicio.js
@@ -21,9 +21,16 @@
 const juegos = {
     aguila: 0,
     sol: 1,
-    tirarMoneda: undefined
+    tirarMoneda: function() {
+        const resultado = Math.random();
+        if (resultado <= 0.5) {
+            console.log("Cae águila 🦅");
+        } else {
+            console.log("Cae sol 🌞");
+        }
+    }
 }
 
 for (let tiros = 0; tiros < 10; tiros++) {
-    // juegos.tirarMoneda()
+    juegos.tirarMoneda();
 }
